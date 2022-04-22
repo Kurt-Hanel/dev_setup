b005d372273152b3573719ceb8a36b644b888e84
Kurt Hanel <hanel.kurt@gmail.com>
2022-04-22 13:52:20 -0500

    Add ps profile

--- .vimrc
+++ .vimrc
@@ -15,28 +15,23 @@ Plug 'tpope/vim-surround'
 Plug 'tpope/vim-fugitive'
 Plug 'easymotion/vim-easymotion'
 Plug 'scrooloose/nerdcommenter'
-Plug 'davidhalter/jedi-vim'
 Plug 'morhetz/gruvbox'
+Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
 call plug#end()
 
 map <silent> <C-n> :NERDTreeToggle<CR>
 map ; :Files<CR>
 nmap s <Plug>(easymotion-s)
-"map <c-n> :nerdtreetoggle<CR>
 set clipboard=unnamedplus
-"map <silent> <C-n> :NERDTreeFocus<CR>
-"syntax on
-"set tw=120
-"set cc=120
-"set hi ColorColumn ctermbg=lightblue guibg=lightblue
 set number
 set spell
 set mouse=a
 
+set t_Co=256
 colorscheme gruvbox
 set background=dark    " Setting dark mode
 
 highlight Visual cterm=reverse ctermbg=NONE
 
-autocmd VimEnter *.py :set cc=120
-autocmd VimEnter *.py :hi ColorColumn ctermbg=darkgreen guibg=red
+"autocmd VimEnter *.py :set cc=120
+"autocmd VimEnter *.py :hi ColorColumn ctermbg=darkgreen guibg=red
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,87 @@
+~\AppData\Local\Programs\oh-my-posh\bin\oh-my-posh.exe --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# Functions
+function pretty-ls($path)
+{
+	Get-ChildItem $path | Format-Wide -Column 3
+}
+
+function la($path)
+{
+	Get-ChildItem $path
+}
+
+function profile-path
+{
+	echo $PROFILE
+}
+
+function open-profile
+{
+	vim $PROFILE
+}
+
+function note
+{
+	$currentDayName = Get-Date -UFormat %A
+	$currentDay = Get-Date -UFormat %d
+	$currentMonth = (Get-Culture).DateTimeFormat.GetMonthName((Get-Date -UFormat %m))
+	$currentMonthLower = ($currentMonth).ToLower()
+	$currentYear = Get-Date -UFormat %Y
+
+	$file = "$HOME\notes\$currentYear\$currentMonthLower\$currentDay.md"
+
+	# check if the file exists already, if not, load template
+	if(-not(Test-Path $file))
+	{
+		try 
+		{
+			$template = "# $currentDayName ($currentMonth $currentDay, $currentYear)`n`n## Morning`n`n- [ ]`n`n## Afternoon`n`n- [ ]`n`n"
+        	$template | Out-File -FilePath $file
+			Write-Host "The file [$file] has been created."
+     	}
+     	catch 
+		{
+        	throw $_.Exception.Message
+     	}
+	}
+
+	vim $file
+}
+
+function notes
+{
+	$currentMonth = Get-Date -UFormat %m
+	$currentMonth = (Get-Culture).DateTimeFormat.GetMonthName($currentMonth).ToLower()
+	$currentMonthLower = ($currentMonth).ToLower()
+	$currentYear = Get-Date -UFormat %Y
+
+	cd $HOME\notes\$currentYear\$currentMonth
+}
+
+function note-summary
+{
+	$currentMonth = Get-Date -UFormat %m
+	$currentMonth = (Get-Culture).DateTimeFormat.GetMonthName($currentMonth).ToLower()
+	$currentYear = Get-Date -UFormat %Y
+	
+	$summary = "summary-$currentMonth-$currentYear.md"
+
+    Out-File -FilePath $summary
+
+	Get-ChildItem -Exclude $summary  | Get-ChildItem -Filter *.md | 
+	Foreach-Object -Process { Add-Content -Path $summary -Value (Get-Content $_) }
+
+	# ConvertFrom-Markdown -Path $summary -OutputType PDF 
+
+	Write-Host "|--> [$summary] has been created."
+	# Write-Host "|--> [summary-$currentMonth-$currentYear.pdf] has been created."
+	Write-Host "|--> Done."
+}
+
+# Aliases 
+Set-Alias -Name ls -Value pretty-ls
+Set-Alias -Name profile -Value open-profile
+Set-Alias -Name nt -Value note
